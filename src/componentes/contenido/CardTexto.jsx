c239e89ba781c0f12e86440cccc063616976dd9a
Juan_Zoloaga <juanpablozoloaga@hotmail.com.ar>
2023-10-07 18:42:39 -0300

    1842

--- src/componentes/contenido/CardTexto.jsx
+++ src/componentes/contenido/CardTexto.jsx
@@ -1,40 +1,58 @@
-import React, {useState} from 'react';
-// import { instance } from '../../config/axiosapi';
+import React, { useState } from "react";
+
+console.log(process.env.REACT_APP_API_KEY_GPT);
 
 function CardTexto({ setChatGPTResponse }) {
-    const [inputUsuario, setInputUsuario] = useState(""); // camel case, snake case, pascal case
-    
-    
-    function changeinput(event){
-        const input = event.target.value;
-        setInputUsuario(input);
-    }  
+  const [inputUsuario, setInputUsuario] = useState(""); // camel case, snake case, pascal case
 
-    const handleClick = async (param) => {
-        try {
-            // const axiosResponse = await instance.get(`/gpt/${param}?prompt=${inputUsuario}`);
-            const response = await fetch(`https://alquilarya.up.railway.app/api/gpt/${param}?prompt=${inputUsuario}`);
-            const json = await response.json();
-            console.log('json', json);            
-            setChatGPTResponse(json.text);
-        } catch(err) {
-            console.log(err);
+  function changeinput(event) {
+    const input = event.target.value;
+    setInputUsuario(input);
+  }
+
+  const handleClick = async (param) => {
+    try {
+      // const axiosResponse = await instance.get(`/gpt/${param}?prompt=${inputUsuario}`);
+      const response = await fetch(
+        `https://alquilarya.up.railway.app/api/gpt/${param}?prompt=${inputUsuario}`,
+        {
+          headers: {
+            Authentication: `Bearer ${process.env.REACT_APP_API_KEY_GPT}`,
+          },
         }
-    } // Refactorizar
+      );
+      const json = await response.json();
+      console.log("json", json);
+      setChatGPTResponse(json.text);
+    } catch (err) {
+      console.log(err);
+    }
+  }; // Refactorizar
 
-    return (
-        <div className='card w-50'> 
-            <header style={{color:"white"}}>Usuario</header>
-            <textarea onChange={changeinput} name="" id="" cols="30" rows="10" placeholder='Ingrese lo que recuerde del sueño'>
-
-            </textarea>
-            <div className='container d-flex col-3 w-100 justify-content-center'>
-                <button onClick={() => handleClick(1)} className='btn btn-primary'>finalizar Sueño</button>
-                <button onClick={() => handleClick(2)} className='btn btn-primary'>Finalizar Narrativa</button>
-                <button onClick={() => handleClick(3)} className='btn btn-primary'>Finalizar....</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="card w-50">
+      <header style={{ color: "white" }}>Usuario</header>
+      <textarea
+        onChange={changeinput}
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="Ingrese lo que recuerde del sueño"
+      ></textarea>
+      <div className="container d-flex col-3 w-100 justify-content-center">
+        <button onClick={() => handleClick(1)} className="btn btn-primary">
+          finalizar Sueño
+        </button>
+        <button onClick={() => handleClick(2)} className="btn btn-primary">
+          Finalizar Narrativa
+        </button>
+        <button onClick={() => handleClick(3)} className="btn btn-primary">
+          Finalizar....
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CardTexto;
